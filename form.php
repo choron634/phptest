9c91e440eee1bb4563ca65d7144f9311f4d40ab6
choron634 <h.syou94@gmail.com>
2018-11-30 00:57:32 +0900

    wa-i

--- form.php
+++ form.php
@@ -1,8 +1,24 @@
-<?php
-  $file_name = "received_text.txt";
-  $name = $_POST["name"]
-  $comment = $_POST["comment"]
-  $count = 
+  <?php
+    //ファイルの中身を読む
+      $file_name = "count.txt";
 
-  file_put_contents("$file_name", "", FILE_APPEND);
- ?>
+      $count = file_get_contents($file_name);
+    //1を足す
+      $count += $count + 1;
+
+    //上書きする
+      $a = fopen("count.txt", "w");
+      @fwrite($a, "$count");
+      fclose($a);
+
+      $name = $_POST['name'];
+      $comment = $_POST['comment'];
+      $data = data('Y-m-d');
+
+
+
+      //$contents = "$count.'<>'.$name.'<>'.$comment.'<>'.$data"
+
+      file_put_contents('received_text.txt', "yahoo!", FILE_APPEND);
+
+  ?>
---
+++ test.php
@@ -0,0 +1,1 @@
+<?php echo "<html><body>Hello PHP</body></html>"; ?>
